46cc7a7d44f1e3b941c4bb69a3a651699551f395
Syndicate555 <saziz028@uottawa.ca>
2020-05-22 12:32:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.send("Hello World"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
